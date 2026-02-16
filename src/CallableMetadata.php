f97331be6e95ad1b907b87d9a855c8c4435abd77
Yonel Ceruto <yonelceruto@gmail.com>
2026-02-16 20:30:04 +0100

    Add attribute handling methods to `CallableMetadata` and remove `readonly` class modifier

--- src/CallableMetadata.php
+++ src/CallableMetadata.php
@@ -2,16 +2,52 @@
 
 namespace OpenSolid\CallableInvoker;
 
-final readonly class CallableMetadata
+final class CallableMetadata
 {
+    /**
+     * @var array<string, mixed>
+     */
+    private array $attributes = [];
+
     /**
      * @param array<string, mixed> $context
      * @param list<string>         $groups
      */
     public function __construct(
-        public \ReflectionFunction $function,
-        public array $context,
-        public array $groups,
+        public readonly \ReflectionFunction $function,
+        public readonly array $context,
+        public readonly array $groups,
     ) {
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    public function hasAttribute(string $name): bool
+    {
+        return \array_key_exists($name, $this->attributes);
+    }
+
+    public function setAttribute(string $name, mixed $value): void
+    {
+        $this->attributes[$name] = $value;
+    }
+
+    public function getAttribute(string $name, ?\Closure $create = null): mixed
+    {
+        if (!$this->hasAttribute($name)) {
+            if (null === $create) {
+                throw new \InvalidArgumentException(\sprintf('Attribute "%s" does not exist.', $name));
+            }
+
+            return $this->attributes[$name] = $create();
+        }
+
+        return $this->attributes[$name];
     }
 }
